85066cee7ed99cb330ae7398725f63ba0a56004d
henriqueinonhe <henriqueinonhe@gmail.com>
2017-07-09 19:03:28 -0300

    Nav Bar Animated

--- js/script.js
+++ js/script.js
@@ -1,4 +1,35 @@
 $(document).ready(main);
 
 function main() {
+	let navToggled = false;
+	$(document).scroll(function() {
+		if(window.pageYOffset > 150 && !navToggled) {
+			$("#logo").css('background-image', 'url(img/logo2.png)');
+			$("#logo").animate({
+				padding: "30px 75px 30px 75px"},
+				400, function() {
+				/* stuff to do after animation is complete */
+			});
+			$("nav ul").animate({
+				paddingTop: "5px",},
+				400, function() {
+				/* stuff to do after animation is complete */
+			});
+			navToggled = true;
+		}
+		else if(window.pageYOffset <= 150 && navToggled) {
+			$("#logo").css('background-image', 'url(img/logo.png)');
+			$("#logo").animate({
+				padding: "50px 100px"},
+				400, function() {
+				/* stuff to do after animation is complete */
+			});
+			$("nav ul").animate({
+				paddingTop: "0",},
+				400, function() {
+				/* stuff to do after animation is complete */
+			});
+			navToggled = false;
+		}
+	});
 }
