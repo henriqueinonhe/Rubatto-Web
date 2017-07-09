1be27a8fc065a89c68bd3a8613daad45c8845441
henriqueinonhe <henriqueinonhe@gmail.com>
2017-07-09 19:27:33 -0300

    Animação Membros

--- js/script.js
+++ js/script.js
@@ -1,9 +1,16 @@
 $(document).ready(main);
 
 function main() {
+	animateNavBar();
+	animateFotos();
+}
+
+function animateNavBar() {
+
 	let navToggled = false;
 	$(document).scroll(function() {
-		if(window.pageYOffset > 150 && !navToggled) {
+//		$("#sensor").html(window.pageYOffset);
+		if(window.pageYOffset > 200 && !navToggled) {
 			$("#logo").css('background-image', 'url(img/logo2.png)');
 			$("#logo").animate({
 				padding: "30px 75px 30px 75px"},
@@ -30,6 +37,26 @@ function main() {
 				/* stuff to do after animation is complete */
 			});
 			navToggled = false;
+		}
+	});
+}
+
+function animateFotos() {
+	$(window).scroll(function() {
+		if(window.pageYOffset >= 300) {
+			$("#boaz").animate({left: "0"}, 600, function() {
+				$(window).scroll(function() {
+					if(window.pageYOffset >= 450) {
+						$("#henrique").animate({right: "0"}, 600, function() {
+							$(window).scroll(function() {
+								if(window.pageYOffset >= 600) {
+									$("#daniel").animate({left: "0"}, 600);
+								}
+							});
+						});
+					}
+				});
+			});
 		}
 	});
 }
